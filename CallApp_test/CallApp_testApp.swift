2ee60a85b1fb47e667c0c7b1ecc1557e0df8983e
Atsushi Hosaka <hosaka2917@gmail.com>
2024-06-26 17:41:26 +0900

    Initial Commit

---
+++ CallApp_test/CallApp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallApp_testApp.swift
+//  CallApp_test
+//
+//  Created by 保坂篤志 on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CallApp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallApp_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CallApp_test
+//
+//  Created by 保坂篤志 on 2024/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
